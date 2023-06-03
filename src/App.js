d4bac2c71c263c871724ec0331fbf29bcd70734f
r4vc31 <r4vc31@gmail.com>
2023-06-03 11:34:30 -0500

    list of Posts and individual posts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home.jsx"
 import Sobre from "./pages/Sobre"
 import Page404 from './pages/Page404';
 import Header from './components/Header';
+import Post from './pages/Post';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sobre' element={<Sobre />} />
+        <Route path='posts/:id' element={<Post />} />
         <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
