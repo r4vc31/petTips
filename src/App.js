16c81481ee159532b04f0d5c5b1bce2414ad522e
Ronald A. Vera C <114179022+r4vc31@users.noreply.github.com>
2023-10-30 22:51:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from "./pages/Home.jsx"
-import Sobre from "./pages/Sobre"
-import Page404 from './pages/Page404';
-import Header from './components/Header';
-import Post from './pages/Post';
-import Categoria from './pages/Categoria';
+import Sobre from "./pages/Sobre.jsx"
+import Page404 from './pages/Page404.jsx';
+import Header from './components/Header.jsx';
+import Post from './pages/Post.jsx';
+import Categoria from './pages/Categoria.jsx';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
